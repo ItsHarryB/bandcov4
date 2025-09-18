c941cc06dd95999ec5ab632147d90fbbd6e62d7e
ItsHarryB <hbemails@pm.me>
2025-09-18 15:24:44 +0100

    theming

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  site: "https://test.brightonandco.co.uk",
+
+  // @ts-ignore
+  adapter: cloudflare(),
+
+  integrations: [preact()]
 });
