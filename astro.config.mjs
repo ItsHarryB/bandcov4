446b8d36b53d06d0e247ce8ce7788a406f31039e
ItsHarryB <hbemails@pm.me>
2025-09-18 15:26:25 +0100

    retry preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,6 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://test.brightonandco.co.uk",
-
-  // @ts-ignore
   adapter: cloudflare(),
-
   integrations: [preact()]
 });
