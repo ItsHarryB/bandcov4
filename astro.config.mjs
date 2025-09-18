9d761988ce342011f35727dc7e9954946d17421b
ItsHarryB <hbemails@pm.me>
2025-09-18 15:28:31 +0100

    retrying preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
