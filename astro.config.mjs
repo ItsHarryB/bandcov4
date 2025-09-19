bb6e994ea995fd9ea4efb1922472c58885b31c06
ItsHarryB <hbemails@pm.me>
2025-09-19 22:07:10 +0100

    defined react

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import preact from '@astrojs/preact';
 
 import sitemap from '@astrojs/sitemap';
 
+import react from '@astrojs/react'
+
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
