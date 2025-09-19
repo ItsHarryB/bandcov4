6dc04ce2dd1fa498a95f5da4518fc3b6d8c37ba8
ItsHarryB <hbemails@pm.me>
2025-09-19 22:46:38 +0100

    minor formatting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Social from "./Social.jsx";
-import "../styles/global.css"; // make sure global.css is imported
+import "../styles/global.css"; // Make sure your global.css is imported
 
 export default function Footer() {
   return (
@@ -9,14 +9,16 @@ export default function Footer() {
       <link rel="sitemap" href="/sitemap-index.xml" />
 
       <div className="footer-container">
-        {/* Left: Copyright */}
-        <div className="footer-section">
-          <p>&copy; {new Date().getFullYear()} Brighton and Co. All rights reserved.</p>
+        {/* Left Column: Copyright */}
+        <div className="footer-section footer-left">
+          <p>
+            &copy; {new Date().getFullYear()} TEST: Brighton and Co. <br />
+          All rights reserved.</p>
         </div>
 
-        {/* Middle: Social Media */}
-        <div className="footer-section">
-          <h4>Follow Me</h4>
+        {/* Middle Column: Social Links */}
+        <div className="footer-section footer-middle">
+          <h4>Follow Me:</h4>
           <div className="social-links">
             <Social platform="Twitter" username="ItsHarryB" />
             <Social platform="GitHub" username="ItsHarryB_" />
@@ -27,9 +29,9 @@ export default function Footer() {
           </div>
         </div>
 
-        {/* Right: Sitemap */}
-        <div className="footer-section">
-          <h4>Sitemap</h4>
+        {/* Right Column: Sitemap */}
+        <div className="footer-section footer-right">
+          <h4>Sitemap:</h4>
           <ul>
             <li><a href="/">Home</a></li>
             <li><a href="/enquiries/">Enquiries</a></li>
@@ -44,7 +46,7 @@ export default function Footer() {
       {/* Footer Meta */}
       <div className="footer-meta">
         <p>
-          TEST: Brighton and Co Website - Made by Harry Brighton | Version 0.0.1 / 19/09/2025
+          TEST: Brighton and Co Website – Made by Harry Brighton | Version 0.0.1 / 19/09/2025
         </p>
       </div>
     </footer>
