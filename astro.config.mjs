e1aeac9431647f7e188cb2c0944967e46f7c613c
ItsHarryB <hbemails@pm.me>
2025-09-19 20:57:34 +0100

    swup implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import cloudflare from '@astrojs/cloudflare';
 
 import preact from '@astrojs/preact';
 
+import Swup from 'swup';
+const swup = new Swup();
+
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
