cd82967b048133e5621ad8ad27372a916f90dd68
ItsHarryB <hbemails@pm.me>
2025-09-19 21:00:32 +0100

    uninstalled swup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,6 @@ import cloudflare from '@astrojs/cloudflare';
 
 import preact from '@astrojs/preact';
 
-import Swup from 'swup';
-const swup = new Swup();
-
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
