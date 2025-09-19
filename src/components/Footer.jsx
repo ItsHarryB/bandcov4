9ebe24aa72ea9ec4b005c804040c52daa53e0c4b
ItsHarryB <hbemails@pm.me>
2025-09-19 22:40:16 +0100

    reformat footer for react

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,66 +1,51 @@
 import React from "react";
 import Social from "./Social.jsx";
-import "../styles/global.css"; // ensure global styles are loaded
+import "../styles/global.css"; // make sure global.css is imported
 
 export default function Footer() {
   return (
-    <footer
-      className="site-footer"
-      style={{
-        padding: "2rem 1rem",
-        backgroundColor: "#f8f8f8",
-        color: "#333",
-        textAlign: "center",
-        transition: "background-color 0.5s ease, color 0.5s ease",
-      }}
-    >
-      {/* Sitemap Section */}
-      <div
-        className="footer-sitemap"
-        style={{
-          marginBottom: "1rem",
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "center",
-          gap: "1rem",
-        }}
-      >
-        <a href="/">Home</a>
-        <a href="/enquiries/">Enquiries</a>
-        <a href="/about/">About Me</a>
-        <a href="/blog/">Blog</a>
-        <a href="/photography/">Photography</a>
-        <a href="/links/">Links</a>
+    <footer className="site-footer">
+      {/* Sitemap link for crawlers */}
+      <link rel="sitemap" href="/sitemap-index.xml" />
+
+      <div className="footer-container">
+        {/* Left: Copyright */}
+        <div className="footer-section">
+          <p>&copy; {new Date().getFullYear()} Brighton and Co. All rights reserved.</p>
+        </div>
+
+        {/* Middle: Social Media */}
+        <div className="footer-section">
+          <h4>Follow Me</h4>
+          <div className="social-links">
+            <Social platform="Twitter" username="ItsHarryB" />
+            <Social platform="GitHub" username="ItsHarryB_" />
+            <Social
+              platform="LinkedIn"
+              url="https://www.linkedin.com/in/harry-brighton-8a2b971a4/"
+            />
+          </div>
+        </div>
+
+        {/* Right: Sitemap */}
+        <div className="footer-section">
+          <h4>Sitemap</h4>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/enquiries/">Enquiries</a></li>
+            <li><a href="/about/">About Me</a></li>
+            <li><a href="/blog/">Blog</a></li>
+            <li><a href="/photography/">Photography</a></li>
+            <li><a href="/links/">Links</a></li>
+          </ul>
+        </div>
       </div>
 
-      {/* Social Icons */}
-      <div
-        className="footer-social"
-        style={{
-          marginBottom: "1rem",
-          display: "flex",
-          justifyContent: "center",
-          gap: "1rem",
-        }}
-      >
-        <Social platform="Twitter" username="ItsHarryB" />
-        <Social platform="GitHub" username="ItsHarryB_" />
-        <Social
-          platform="LinkedIn"
-          url="https://www.linkedin.com/in/harry-brighton-8a2b971a4/"
-        />
-      </div>
-
-      {/* Footer Info */}
-      <div
-        className="footer-info"
-        style={{
-          fontSize: "0.875rem",
-          color: "#666",
-          lineHeight: "1.4",
-        }}
-      >
-        TEST: Brighton and Co Website – Made by Harry Brighton | Version 0.0.1 / 19/09/2025
+      {/* Footer Meta */}
+      <div className="footer-meta">
+        <p>
+          TEST: Brighton and Co Website - Made by Harry Brighton | Version 0.0.1 / 19/09/2025
+        </p>
       </div>
     </footer>
   );
