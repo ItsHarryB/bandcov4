13e3ee5f4919c83ebcf7d84af1c4dfebd4807342
ItsHarryB <hbemails@pm.me>
2025-09-19 22:21:37 +0100

    removed greeting

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "preact";
 import { Twitter, Github, Linkedin } from "lucide-react";
 import "../styles/global.css";
 
