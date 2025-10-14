0dcacf991aab025891c8976fd0c10b3b9f90a442
ItsHarryB <hbemails@pm.me>
2025-10-14 18:47:42 +0100

    mobile changes v0.5.0d

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -55,7 +55,7 @@ export default function Footer() {
       {/* Footer meta */}
       <div className="footer-meta">
         <p>
-          TEST: Brighton and Co Website – Made by Harry Brighton | Version 0.5.0b - 14/10/2025
+          TEST: Brighton and Co Website – Made by Harry Brighton | Version 0.5.0d - 14/10/2025
         </p>
       </div>
     </footer>
