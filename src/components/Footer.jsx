c8820755a858b8c886b03f40f0e62e4ed33009a9
ItsHarryB <hbemails@pm.me>
2025-10-20 18:42:21 +0100

    enquiries form formatting v0.7.1a

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -55,7 +55,7 @@ export default function Footer() {
       {/* Footer meta */}
       <div className="footer-meta">
         <p>
-          TEST: Brighton and Co Website – Made by Harry Brighton | Version 0.7.1 - 20/10/2025
+          TEST: Brighton and Co Website – Made by Harry Brighton | Version 0.7.1a - 20/10/2025
         </p>
       </div>
     </footer>
