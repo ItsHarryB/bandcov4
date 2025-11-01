5c0082a3f19b14a458f8d37ce9ceb0b8e0c9b680
ItsHarryB <github@h.brightonandco.co.uk>
2025-11-01 23:37:27 +0000

    minor header fix, updated to v0.8.1a

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -55,7 +55,7 @@ export default function Footer() {
       {/* Footer meta */}
       <div className="footer-meta">
         <p>
-          TEST: Brighton and Co Website – Made by Harry Brighton | Version 0.8.1 - 01/11/2025
+          TEST: Brighton and Co Website – Made by Harry Brighton | Version 0.8.1a - 01/11/2025
         </p>
       </div>
     </footer>
