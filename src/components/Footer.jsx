a20d0c50b74514679b21305272d8d2d87472c7fd
ItsHarryB <hbemails@pm.me>
2025-11-04 21:01:56 +0000

    changed home page text, v0.10.2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -137,7 +137,7 @@ export default function Footer() {
       {/* Footer meta */}
       <div className="footer-meta" style={{ marginTop: "1.25rem" }}>
         <p>
-          TEST: Brighton and Co Website – Made by Harry Brighton | Version 0.10.1 - 04/11/2025
+          TEST: Brighton and Co Website – Made by Harry Brighton | Version 0.10.2 - 04/11/2025
         </p>
       </div>
     </footer>
