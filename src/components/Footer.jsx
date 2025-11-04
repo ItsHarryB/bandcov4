28e3b33ecf5d9187acd803064def4401086b8a1a
ItsHarryB <hbemails@pm.me>
2025-11-04 18:17:06 +0000

    header formatting, v0.9.1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -135,7 +135,7 @@ export default function Footer() {
       {/* Footer meta */}
       <div className="footer-meta" style={{ marginTop: "1.25rem" }}>
         <p>
-          TEST: Brighton and Co Website – Made by Harry Brighton | Version 0.9.0 - 04/11/2025
+          TEST: Brighton and Co Website – Made by Harry Brighton | Version 0.9.1 - 04/11/2025
         </p>
       </div>
     </footer>
