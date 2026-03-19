ba2a837c095c8bfe0a72e4e58b6a14669621cc80
ItsHarryB <github@h.brightonandco.co.uk>
2026-03-19 17:24:43 +0000

    backend updates, v4.0.6a

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -195,7 +195,7 @@ export default function Footer() {
       {/* Footer meta */}
       <div className="footer-meta" style={{ marginTop: "1.25rem" }}>
         <p>
-          Brighton and Co Website – Made by Harry Brighton | Version 4.0.6 - 10/03/2026
+          Brighton and Co Website – Made by Harry Brighton | Version 4.0.6a - 10/03/2026
         </p>
       </div>
     </footer>
