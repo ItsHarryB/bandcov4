ab3a781c63c1d95e8bda0068ab5d93a0bd49393b
ItsHarryB <github@h.brightonandco.co.uk>
2026-05-03 17:12:48 +0100

    more csp changes to fix wcb, v26.5.2a

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,35 +6,6 @@ import mdx from '@astrojs/mdx';
 import preact from '@astrojs/preact';
 
 export default defineConfig({
-  security: {
-    csp: {
-      // 1. REMOVE the algorithm property entirely!
-      // This stops Astro from automatically hashing inline styles and scripts.
-      
-      scriptDirective: {
-        resources: [
-          "'self'", 
-          "'unsafe-inline'", // Now this actually works because there are no hashes!
-          "'unsafe-eval'", 
-          "https://unpkg.com", 
-          "https://static.cloudflareinsights.com",
-          "https://api.websitecarbon.com"
-        ]
-      },
-      
-      styleDirective: {
-        resources: [
-          "'self'", 
-          "'unsafe-inline'", // This now allows Preact to use style="..." attributes!
-          // We no longer need 'unsafe-hashes' because we aren't using hashes anymore
-        ]
-      },
-      
-      directives: [
-        "connect-src 'self' https://cloudflareinsights.com https://api.websitecarbon.com"
-      ]
-    }
-  },
   adapter: cloudflare({
     imageService: 'compile', 
   }),
